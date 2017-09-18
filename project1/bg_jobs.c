5bd71e0ebf5c828e729b6dd245e55e894d6af37f
JulianDomingo <julian.domingo@utexas.edu>
2017-09-18 12:36:17 -0500

    Fixed seg fault error when removing linked list node from bg list.

--- project1/bg_jobs.c
+++ project1/bg_jobs.c
@@ -31,41 +31,23 @@ bg_jobs_linked_list_node_t *create_linked_list_node(process_group_t *process_gro
 void remove_linked_list_node(process_group_t *process_group_to_remove, bg_jobs_linked_list_t *linked_list) {
     if (linked_list->size == 0) { return; }
 
-    bg_jobs_linked_list_node_t *temp_pointer;
+    bg_jobs_linked_list_node_t *runner = linked_list->pointer_to_head->next;
+    while (!runner->is_head_or_tail) {
+        if (runner->process_group == process_group_to_remove) {
+            // Save the pointer to the node to destroy before rerouting pointers.
+            temp_pointer = runner;
 
-    // Process group to remove is the most recent background job
-    if (process_group_to_remove == linked_list->pointer_to_head->next->process_group) {
-        // Save the pointer to the node to destroy before rerouting pointers.
-        temp_pointer = linked_list->pointer_to_head->next;
+            // Remove any pointers to the node to destroy.
+            runner->previous->next = runner->next; 
+            runner->next->previous = runner->previous;
 
-        // Remove any pointers to the node to destroy.
-        linked_list->pointer_to_head->next = linked_list->pointer_to_head->next->next;
-        linked_list->pointer_to_head->next->next->previous = linked_list->pointer_to_head;
+            // First free the heap-stored members of process group before freeing linked_list node itself.
+            destroy_process_group(temp_pointer->process_group);
+            free(temp_pointer);
+        }
 
-        // First free the heap-stored members of process group before freeing linked_list node itself.
-        destroy_process_group(temp_pointer->process_group);
-        free(temp_pointer);
-    }
-    else {
-        // Process group to remove is not the most recent background job
-        bg_jobs_linked_list_node_t *runner = linked_list->pointer_to_head->next;
-        while (!runner->is_head_or_tail) {
-            if (runner->process_group == process_group_to_remove) {
-                // Save the pointer to the node to destroy before rerouting pointers.
-                temp_pointer = runner;
-
-                // Remove any pointers to the node to destroy.
-                runner->previous->next = runner->next; 
-                runner->next->previous = runner->previous;
-
-                // First free the heap-stored members of process group before freeing linked_list node itself.
-                destroy_process_group(temp_pointer->process_group);
-                free(temp_pointer);
-            }
-
-            runner = runner->next;
-        } 
-    }
+        runner = runner->next;
+    } 
 
     linked_list->size--;
 }
